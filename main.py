1678b784eab5ffc641bf5c77cb892d8f2d539376
Oooooiii <hearong.200@gmail.com>
2022-05-11 01:01:46 +0700

    lesson 3 completed

--- main.py
+++ main.py
@@ -4,38 +4,50 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QIcon
 
 
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(600, 200, 500, 500)
-    win.setWindowTitle("Restricted")
-    win.setWindowIcon(QIcon("Sanity.png"))
-    win.setToolTip("Restricted")
+class restricted(QMainWindow):
+    def __init__(self):
+        super(restricted, self).__init__()
+        self.setGeometry(600, 200, 500, 500)
+        self.setWindowTitle("Restricted")
+        self.setToolTip("Restricted")
+        self.setWindowIcon(QIcon("Sanity.png"))
+        self.initUI()
 
-    lbl_name = QtWidgets.QLabel(win)
-    lbl_name.setText("Enter name : ")
-    lbl_name.move(50, 50)
+    def initUI(self):
+        self.lbl_name = QtWidgets.QLabel(self)
+        self.lbl_name.setText("Enter your name : ")
+        self.lbl_name.move(50, 50)
 
-    lbl_surename = QtWidgets.QLabel(win)
-    lbl_surename.setText("Enter surname : ")
-    lbl_surename.move(50, 90)
+        self.lbl_surname = QtWidgets.QLabel(self)
+        self.lbl_surname.setText("Enter your surname : ")
+        self.lbl_surname.move(50, 90)
 
-    txt_name = QtWidgets.QLineEdit(win)
-    txt_name.move(200, 50)
+        self.txt_name = QtWidgets.QLineEdit(self)
+        self.txt_name.move(200, 50)
+        self.txt_name.resize(200, 32)
 
-    txt_surname = QtWidgets.QLineEdit(win)
-    txt_surname.move(200, 90)
+        self.txt_surname = QtWidgets.QLineEdit(self)
+        self.txt_surname.move(200, 90)
+        self.txt_surname.resize(200, 32)
+
+        self.btn_save = QtWidgets.QPushButton(self)
+        self.btn_save.setText('Save')
+        self.btn_save.clicked.connect(self.clicked)
+        self.btn_save.move(200, 130)
+
+        self.lbl_result = QtWidgets.QLabel(self)
+        self.lbl_result.setText('RESULT')
+        self.lbl_result.move(200, 170)
+        self.lbl_result.resize(200, 200)
 
     def clicked(self):
-        print('button clicked')
-        print('name :' + txt_name.text())
-        print('surname :' + txt_surname.text())
+        self.lbl_result.setText(
+            'Name\t:' + self.txt_name.text() + '\nSurname :' + self.txt_surname.text())
 
-    btn_save = QtWidgets.QPushButton(win)
-    btn_save.setText('Save')
-    btn_save.clicked.connect(clicked)
-    btn_save.move(200, 130)
 
+def window():
+    app = QApplication(sys.argv)
+    win = restricted()
     win.show()
     sys.exit(app.exec_())
 
